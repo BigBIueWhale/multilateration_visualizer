9588cd85b8ed68375db1b92ddeaf9d620cf44ae4
Ronen Zyroff <rzyroff@gmail.com>
2024-03-15 07:22:54 -0700

    Add a todo

--- electron/src/renderer/src/components/MainPageContent/MainPageContent.component.tsx
+++ electron/src/renderer/src/components/MainPageContent/MainPageContent.component.tsx
@@ -1,4 +1,7 @@
 export function MainPageContent() {
+    // TODO: Here we need to implement a visualization like this:
+    // https://threejs.org/docs/#examples/en/controls/OrbitControls
+    // https://threejs.org/examples/#misc_controls_orbit
     return (
         <>
         </>

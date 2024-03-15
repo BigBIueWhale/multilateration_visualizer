66f01df4ceb1dfc38461a9e8e64dcf0918100c98
Ronen Zyroff <rzyroff@gmail.com>
2024-03-15 07:28:41 -0700

    Expand the plan for MainPageContent

--- electron/src/renderer/src/components/MainPageContent/MainPageContent.component.tsx
+++ electron/src/renderer/src/components/MainPageContent/MainPageContent.component.tsx
@@ -2,6 +2,11 @@ export function MainPageContent() {
     // TODO: Here we need to implement a visualization like this:
     // https://threejs.org/docs/#examples/en/controls/OrbitControls
     // https://threejs.org/examples/#misc_controls_orbit
+    // Specifically, the plan is to create a 3d world with a
+    // basic hard-coded floor. Default focal point at .0, .0, .0.
+    // Ability to adjust the focal point using 3 @mui sliders X, Y, Z
+    // and ability to move around the focal point with mouse drag
+    // which is just regular Orbit action.
     return (
         <>
         </>

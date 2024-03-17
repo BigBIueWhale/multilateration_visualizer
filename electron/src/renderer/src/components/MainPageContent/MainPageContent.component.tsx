6a277a992928d1d2db3cfb861a30978d0604e02a
Ronen Zyroff <rzyroff@gmail.com>
2024-03-17 03:29:26 -0700

    Minor- rename variable

--- electron/src/renderer/src/components/MainPageContent/MainPageContent.component.tsx
+++ electron/src/renderer/src/components/MainPageContent/MainPageContent.component.tsx
@@ -6,10 +6,10 @@ import { Slider, Box as MuiBox, Typography } from '@mui/material';
 
 export function MainPageContent() {
   // State for the focal point position
-  const [position, setPosition] = useState(new Vector3(...[0, 0, 0]));
+  const [focalPointPosition, setFocalPointPosition] = useState(new Vector3(...[0, 0, 0]));
 
   const handleSliderChange = (dimension: number, newValue: number) => {
-    setPosition((prevPosition) => {
+    setFocalPointPosition((prevPosition) => {
       const newPosition = [...prevPosition];
       newPosition[dimension] = newValue;
       return new Vector3(...newPosition);
@@ -48,7 +48,7 @@ export function MainPageContent() {
               min={-10}
               max={10}
               step={0.1}
-              value={position.toArray()[i]}
+              value={focalPointPosition.toArray()[i]}
               onChange={(_, newValue) => {
                   if (typeof newValue === 'number') {
                       handleSliderChange(i, newValue)
@@ -69,13 +69,13 @@ export function MainPageContent() {
           {/* Directional light for shadows */}
           <directionalLight position={[0, 10, 5]} intensity={1} />
           {/* Focal point object */}
-          <Box position={position} />
+          <Box position={focalPointPosition} />
           {/* Floor */}
           <Plane args={[3.0, 3.0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
             <meshStandardMaterial side={DoubleSide} attach="material" color="lightgrey" />
           </Plane>
           {/* Orbit Controls */}
-          <OrbitControls target={position} />
+          <OrbitControls target={focalPointPosition} />
         </Canvas>
       </MuiBox>
     </MuiBox>

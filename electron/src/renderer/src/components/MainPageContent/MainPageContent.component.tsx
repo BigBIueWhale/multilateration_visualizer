23d14075b2131f8fcf746f149fd226d56fa11a02
Ronen Zyroff <rzyroff@gmail.com>
2024-03-17 04:32:14 -0700

    Try to make double-click to reset work

--- electron/src/renderer/src/components/MainPageContent/MainPageContent.component.tsx
+++ electron/src/renderer/src/components/MainPageContent/MainPageContent.component.tsx
@@ -6,6 +6,7 @@ import { Box as MuiBox } from '@mui/material';
 
 export function MainPageContent() {
   const [canvasHeight, setCanvasHeight] = useState(0);
+  const [cameraTarget, setCameraTarget] = useState(new Vector3(0, 0, 0));
   const canvasRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -24,23 +25,34 @@ export function MainPageContent() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  const handleDoubleClick = () => {
+    setCameraTarget(new Vector3(0, 0, 0));
+  };
+
   return (
     <MuiBox component="div" display="flex" flexDirection="column" height="100%">
       <MuiBox ref={canvasRef} component="div" display="flex">
         {/* Adjust Canvas style to use calculated height */}
-        <Canvas style={{ width: '100%', height: `${canvasHeight}px`, minHeight: '0px' }}>
+        <Canvas
+          style={{ width: '100%', height: `${canvasHeight}px`, minHeight: '0px' }}
+          onDoubleClick={handleDoubleClick}
+        >
           {/* Ambient light to illuminate the scene */}
           <ambientLight intensity={0.5} />
+
           {/* Directional light for shadows */}
           <directionalLight position={[0, 10, 5]} intensity={1} />
+
           {/* Focal point object */}
-          <Box position={new Vector3(...[0, 0, 0])} />
+          <Box position={cameraTarget} />
+
           {/* Floor */}
           <Plane args={[3.0, 3.0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
             <meshStandardMaterial side={DoubleSide} attach="material" color="lightgrey" />
           </Plane>
+
           {/* Orbit Controls */}
-          <OrbitControls target={new Vector3(...[0, 0, 0])} />
+          <OrbitControls target={cameraTarget} />
         </Canvas>
       </MuiBox>
     </MuiBox>

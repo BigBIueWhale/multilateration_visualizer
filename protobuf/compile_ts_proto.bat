0571cead21f3363380213d8c90de3afe7bf3cc5e
Ronen Zyroff <85248530+BigBIueWhale@users.noreply.github.com>
2024-05-11 04:09:38 +0300

    Generate a separate specialized .ts file that includes the grpc-js stuff, this will allow us to include types from the ts-proto generated file without getting an error due to missing nodejs libraries.

--- protobuf/compile_ts_proto.bat
+++ protobuf/compile_ts_proto.bat
@@ -1,5 +1,8 @@
 @echo off
 
+REM Store the current directory
+SET "original_dir=%CD%"
+
 REM The purpose here is to avoid code duplication.
 REM Define the types and gRPC API once in the .proto file, and then generate idiomatic TypeScript code, and Rust code!
 
@@ -9,10 +12,25 @@ echo Navigate to the folder in case the user ran this batch file from root direc
 cd protobuf
 
 echo Checking for grpc_api.ts
-if exist "%CD%\..\electron\src\shared\src\proto\grpc_api.ts" (
-    echo Deleting "%CD%\..\electron\src\shared\src\proto\grpc_api.ts"
-    del "%CD%\..\electron\src\shared\src\proto\grpc_api.ts"
+if exist "%original_dir%\..\electron\src\shared\src\proto\grpc_api.ts" (
+    echo Deleting "%original_dir%\..\electron\src\shared\src\proto\grpc_api.ts"
+    del "%original_dir%\..\electron\src\shared\src\proto\grpc_api.ts"
 )
 
-echo Compiling for TypeScript using ts-proto
-"./precompiled/protobuf_compiler/protoc-25.2/bin/protoc.exe" --plugin=protoc-gen-ts_proto="%CD%/../electron/node_modules/.bin/protoc-gen-ts_proto.cmd" --ts_proto_out="%CD%/../electron/src/shared/src" --ts_proto_opt=outputServices=grpc-js "./proto/grpc_api.proto" --ts_proto_opt=esModuleInterop=true
+echo Checking for grpc_api_with_services.ts
+if exist "%original_dir%\..\electron\src\shared\src\proto\grpc_api_with_services.ts" (
+    echo Deleting "%original_dir%\..\electron\src\shared\src\proto\grpc_api_with_services.ts"
+    del "%original_dir%\..\electron\src\shared\src\proto\grpc_api_with_services.ts"
+)
+
+echo Compiling for TypeScript with services using ts-proto
+"./precompiled/protobuf_compiler/protoc-25.2/bin/protoc.exe" --plugin=protoc-gen-ts_proto="%original_dir%/../electron/node_modules/.bin/protoc-gen-ts_proto.cmd" --ts_proto_out="%original_dir%/../electron/src/shared/src" --ts_proto_opt=outputServices=grpc-js "./proto/grpc_api.proto" --ts_proto_opt=esModuleInterop=true
+REM The reason we need to create 2 generated files is that the version without the services can be
+REM safely imported from the renderer, which would normally cause an error of not having access to
+REM any node.js libraries which are required by the grpc-js client server implementations.
+echo Renaming...
+cd "%original_dir%\..\electron\src\shared\src\proto"
+ren "grpc_api.ts" "grpc_api_with_services.ts"
+cd "%original_dir%"
+echo Compiling for TypeScript without services using ts-proto
+"./precompiled/protobuf_compiler/protoc-25.2/bin/protoc.exe" --plugin=protoc-gen-ts_proto="%original_dir%/../electron/node_modules/.bin/protoc-gen-ts_proto.cmd" --ts_proto_out="%original_dir%/../electron/src/shared/src" "./proto/grpc_api.proto" --ts_proto_opt=esModuleInterop=true
